87fcd5e077b586be24ddbd49249224105a93e03b
Guimeb <rm551981@fiap.com.br>
2023-09-16 17:31:39 -0300

    Diminuido quantia de pastas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
-import NavHeader from "../NavHeader";
-import Burguer from '../Burguer';
-import LogoHeader from "../LogoHeader";
-import LoginHeader from "../LoginHeader";
+import React from 'react';
+import hamburguer from '../../imgs/burguer.png'
+import logo from '../../imgs/galoBanner.png'
 
 const Head = styled.header`
     position: fixed;
@@ -16,11 +15,9 @@ const Head = styled.header`
     justify-content: start;
     background: white;
     box-shadow: 0 10px 50px rgba(0, 0, 0, 0.1);
-    transition-property: translate, opacity, visibility;
+    transition-property: visibility;
     transition-duration: 0.5s;
-    // translate: -100% 0;
     visibility: visible;
-    opacity: 1;
 
     @media (min-width: 1100px) {
           translate: 0 0;
@@ -28,19 +25,123 @@ const Head = styled.header`
           align-items: center;
           justify-content: space-between;
           width: 100%;
-          height: 7em;
+          height: 5.5em;
           padding: 1em;
-          opacity: 1;
           visibility: visible;
+    }
+`
+const Burguer = styled.div`
+    position: absolute;
+    width: 0;
+    height: 0;
+    z-index: 100;
+    top: 2%;
+    left: 5%;
+    visibility: visible;
+    opacity: 1;
+    
+    @media (min-width: 1100px) {
+        visibility: hidden;
+        opacity: 0;
+    }
+`
+const Hamburguer = styled.img`
+    width: 41.17px;
+    cursor: pointer;
+
+`
+const LogoHeader = styled.div`
+display: flex;
+justify-content: space-between;
+`
+const LogoSideBarimg = styled.img`
+    margin: auto auto auto 2em;
+    width: 10em;
+`
+const Nav = styled.div`
+`
+const NavHeader = styled.ul`
+    padding-top: 1.5em;
+
+    li{
+        text-align: start;
+        list-style: none;
+        padding: 0.5em;
+        font-size: 1.3rem;
+    }
+
+    a{
+        text-decoration: none;
+        color: black;
+    }
+
+    @media (min-width: 1100px) {
+        display: flex;
+        padding: 0;
+        text-align: center;
+    }
+`
+const LoginHeader = styled.div`
+    position: absolute;
+    bottom: 2%;
+    text-align: start;
+    padding: 0.5em;
+
+    @media (min-width: 1100px) {
+        position: unset;
+        padding: 0;
+        text-align: center;
+        margin-right: 1em;
+    }
+
+`
+const UnderlineLogin = styled.div`
+    border: 1px solid #000;
+    width: 300%;
+
+    @media (min-width: 1100px) {
+        visibility: hidden;
+        opacity: 0;
+    }
+`
+
+const LoginAnchor = styled.a`
+    text-decoration: none;
+    list-style: none;
+    color: black;
+    font-size: 1.3rem;
 `
 
 function Header() {
     return (
         <Head>
-            <Burguer />
-            <LogoHeader />
-            <NavHeader />
-            <LoginHeader />
+            <Burguer>
+                <Hamburguer
+                    src={hamburguer}
+                    alt='burguer'></Hamburguer>
+            </Burguer>
+            <LogoHeader>
+                <LogoSideBarimg
+                    src={logo}
+                    alt='logo'
+                    className='logo-img'
+                ></LogoSideBarimg>
+            </LogoHeader>
+            <Nav>
+                <NavHeader>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#problema">Problema</a></li>
+                    <li><a href="#causas">Causas</a></li>
+                    <li><a href="#impactos">Impactos</a></li>
+                    <li><a href="#solucao">Solução</a></li>
+                    <li><a href="#quem-somos">Quem somos</a></li>
+                    <li><a href="#equipe">Equipe</a></li>
+                </NavHeader>
+            </Nav>
+            <LoginHeader>
+                <UnderlineLogin></UnderlineLogin>
+                <LoginAnchor id="loginNav" href="./login.html">Login</LoginAnchor>
+            </LoginHeader>
         </Head>
     );
 }
